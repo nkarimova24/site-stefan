8c89f484e070d16967afe07e91ae8cfdc80fc7a7
kw1c-HorstmanKes <kes.horstman@edu-kw1c.nl>
2025-05-19 12:21:12 +0200

    Refactor code structure for improved readability and maintainability

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -6,22 +6,25 @@
     <title>@yield('title', 'My Website')</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class="bg-gray-100 text-gray-800">
-    <header class="bg-blue-600 text-white">
-        <nav class="container mx-auto p-4">
+<body class="bg-black text-gray-800 flex flex-col min-h-screen">
+    <header class="text-white">
+        <nav class="container mx-auto p-4 flex items-center space-x-4" style="background-image: url('{{ asset('nav.png') }}'); background-size: cover; background-position: center;">
+            <a href="/">
+                <img src="{{ asset('../stefanbuslogo.png') }}" alt="stefanbuslogo" class="h-20 w-50 rounded-full">
+            </a>
             <ul class="flex space-x-4">
                 <li><a href="/" class="hover:underline">Home</a></li>
-                <li><a href="/about" class="hover:underline">About</a></li>
-                <li><a href="/contact" class="hover:underline">Contact</a></li>
+                <li><a href="/about" class="hover:underline">Wie ben ik</a></li>
+                <li><a href="/portfolio" class="hover:underline">portfolio</a></li>
             </ul>
         </nav>
     </header>
 
-    <main class="container mx-auto p-6 bg-white shadow-md rounded-lg mt-6">
+    <main class="container mx-auto p-6 shadow-md rounded-lg mt-6 flex-grow">
         @yield('content')
     </main>
 
-    <footer class="bg-gray-800 text-white text-center p-4 mt-6">
+    <footer class="bg-gray-800 text-white text-center p-4">
         <p>&copy; {{ date('Y') }} My Website. All rights reserved.</p>
     </footer>
 </body>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+Route::get('/about', function () {
+    return view('about.index');
+});
+Route::get('/portfolio', function () {
+    return view('portfolio.index');
+});
+
+
